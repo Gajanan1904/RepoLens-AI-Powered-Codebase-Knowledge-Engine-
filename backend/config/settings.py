eb40c501e5df9e1e908d7b506dda5acc180511db
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-07-07 09:23:33 +0530

    settings.py changed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -110,6 +110,9 @@ DATABASES = {
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
         "PORT": config("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
